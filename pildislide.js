98e5a16796df606bc81cf38dd4b689d841f3e3db
Otto-Cristofer Vanasaun <ottovanasaun@gmail.com>
2023-11-25 19:32:04 +0200

    Alternatiivid pmst valmissss!!!

--- pildislide.js
+++ pildislide.js
@@ -5,40 +5,40 @@ var elementHeight = pilt[0].clientHeight;
 
 // animeeri
 function animate(i) {
-  if (i == 1) {
+  if (i % 2 === 1) {
     pilt[i].classList.add('animatevasak');
-  } else if (i === 0 || i === 2) {
+  } else if (i % 2 === 0) {
     pilt[i].classList.add('animateparem');
   }
 }
-
+debugger
 // ootab scrollimist, kutsub välja "animate" funktsiooni
 document.addEventListener('scroll', animate);
-
 // kas elementi on näha?
 function inView() {
   var windowHeight = window.innerHeight;
   // kui palju on keritud?
   var scrollY = window.scrollY;
-  
   // distants lehe ülaservast ekraani alaservani
   var scrollPosition = scrollY + windowHeight;
   // distants ekraani ülaservast elemendi alaservani
   var kaugused = [];
-  for (var i = 0; i <= pilt.length; i++) {
-    var elementPosition = pilt[i].getBoundingClientRect().top + scrollY + elementHeight;
+  for (let i = 0; i < pilt.length; i++) {
+    var elementPosition = pilt[i].getBoundingClientRect().top + scrollY + elementHeight + 100;
     kaugused.push(elementPosition);
   }
 
-  for (var i = 0; i <= kaugused.length; i++) {
+  for (let i = 0; i < kaugused.length; i++) {
     // kumb väärtus on suurem ehk kas elementi on näha?
     if (scrollPosition > kaugused[i]) {
-      animate(i)
+      var s = i;
+      animate(s)
+    }
   }
-  }
-  
-  
-  return false;
+}
+
+window.onscroll = () => {
+  inView()
 }
 
 
@@ -51,3 +51,4 @@ function inView() {
 // viited: https://stackoverflow.com/questions/9090750/how-to-slow-down-a-javascript-loop (20.11.2023)
 // https://codepen.io/jr-cologne/pen/zdYdmx (20.11.2023)
 // https://www.w3schools.com/js/js_if_else.asp (25.11.2023)
+// https://www.youtube.com/watch?v=ABlaMXkUwzY (25.11.2023)
