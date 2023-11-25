7f5ae3d3568db21d6c388104ee9b543db5f1913d
Otto-Cristofer Vanasaun <ottovanasaun@gmail.com>
2023-11-25 02:45:22 +0200

    js katkine aga ma tegelen

--- pildislide.js
+++ pildislide.js
@@ -1,18 +1,20 @@
-var document;
-var vasakultpilt = document.getElementsByClassName('pilt piltvasakul');
-var paremaltpilt = document.getElementsByClassName('pilt piltparemal');
-var elementHeight = vasakultpilt.clientHeight;
 
+var pilt = Array.from(document.getElementsByClassName('pilt'));
+var elementHeight = pilt[0].clientHeight;
+
+
+// animeeri
+function animate(i) {
+  if (i == 1) {
+    pilt[i].classList.add('animatevasak');
+  } else if (i === 0 || i === 2) {
+    pilt[i].classList.add('animateparem');
+  }
+}
 
 // ootab scrollimist, kutsub välja "animate" funktsiooni
+document.addEventListener('scroll', animate);
 
-for (var i = 0; i < vasakultpilt.length; i++) {
-  vasakultpilt[i].addEventListener('scroll', animatevasak);
-}
-
-for (var i = 0; i < paremaltpilt.length; i++) {
-  paremaltpilt[i].addEventListener('scroll', animateparem);
-}
 // kas elementi on näha?
 function inView() {
   var windowHeight = window.innerHeight;
@@ -22,28 +24,30 @@ function inView() {
   // distants lehe ülaservast ekraani alaservani
   var scrollPosition = scrollY + windowHeight;
   // distants ekraani ülaservast elemendi alaservani
-  var elementPosition = vasakultpilt[0].getBoundingClientRect().top + scrollY + elementHeight;
-  
-  // kumb väärtus on suurem ehk kas elementi on näha?
-  if (scrollPosition > elementPosition) {
-    return true;
+  var kaugused = [];
+  for (var i = 0; i <= pilt.length; i++) {
+    var elementPosition = pilt[i].getBoundingClientRect().top + scrollY + elementHeight;
+    kaugused.push(elementPosition);
   }
+
+  for (var i = 0; i <= kaugused.length; i++) {
+    // kumb väärtus on suurem ehk kas elementi on näha?
+    if (scrollPosition > kaugused[i]) {
+      animate(i)
+  }
+  }
+  
   
   return false;
 }
 
-// animeeri, kui elementi on näha
-function animatevasak() {
-  if (inView()) {
-    vasakultpilt[0].classList.add('animatevasak')
-  }
-}
 
-function animateparem() {
-  if (inView()) {
-    paremaltpilt[0].classList.add('animateparem')
-  }
-}
+
+
+
+
+
 
 // viited: https://stackoverflow.com/questions/9090750/how-to-slow-down-a-javascript-loop (20.11.2023)
 // https://codepen.io/jr-cologne/pen/zdYdmx (20.11.2023)
+// https://www.w3schools.com/js/js_if_else.asp (25.11.2023)
